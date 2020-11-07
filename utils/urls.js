ee436d38c4595144bbe96311f7a838f285243b6f
Entreprenerd <valeranijr@gmail.com>
2020-11-07 16:31:01 +0100

    Products page with hardcoded values

---
+++ utils/urls.js
@@ -0,0 +1,19 @@
+export const API_URL =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337"
+
+/**
+ * Given a image object return the proper path to display it
+ * Provides a default as well
+ * @param {any} image 
+ */
+export const fromImageToUrl = (image) => {
+    if (!image) {
+      return "/vercel.svg"; //Or default image here
+    }
+    if (image.url.indexOf("/") === 0) {
+      //It's a relative url, add API URL
+      return `${API_URL}${image.url}`;
+    }
+  
+    return image.url;
+};
