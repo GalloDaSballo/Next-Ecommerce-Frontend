f7997c78c5bc3de263cf028d7a1277cfd247e742
Entreprenerd <valeranijr@gmail.com>
2020-11-08 14:19:04 +0100

    fix sync video

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,35 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+
 import styles from '../styles/Header.module.css'
 
-export default () => (
-    <nav>
-        <div>
+export default () => {
 
+    const router = useRouter()
+    const isHome = router.pathname === "/"
+
+    const goBack = (event) => {
+        event.preventDefault()
+        router.back()
+    }
+
+    return (
+        <div className={styles.nav}>
+            {!isHome && 
+                <div className={styles.back}>
+                    <a href="#" onClick={goBack}>{"<"} Back </a>
+                </div>
+            }
             <div className={styles.title}>
-                <h1>
-                    <Link href="/">
-                        <a>The E-Commerce</a>
-                    </Link>
-                </h1>
+                <Link href="/">
+                    <a>
+                        <h1>
+                            The E-Commerce
+                        </h1>
+                    </a>
+                </Link>
             </div>
 
         </div>
-    </nav>
-)
+    )
+}
